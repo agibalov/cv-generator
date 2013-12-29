d820854596602253c7b62c3055212af73e20adfd
loki2302 <loki2302@loki2302.me>
2013-12-29 17:12:11 +0400

    Now using JSON instead of XML to describe CV contents

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ cv-generator
 
 1. `npm install`
 2. `npm start`
-3. see `resume.html` and `resume.pdf` generated
+3. see `cv.html` and `cv.pdf` generated
 
 Also (thanks to Travis):
 
-1. http://loki2302.me/cv-generator/resume.html
-2. http://loki2302.me/cv-generator/resume.pdf
+1. http://loki2302.me/cv-generator/cv.html
+2. http://loki2302.me/cv-generator/cv.pdf
--- app.js
+++ app.js
@@ -1,68 +1,26 @@
 var path = require("path");
 var fs = require("fs");
-var libxmljs = require("libxmljs");
 var swig = require("swig");
 var childProcess = require("child_process");
 var phantomjs = require("phantomjs");
 var phantomBinPath = phantomjs.path;
 var _ = require("underscore");
 
-var resumeXml = fs.readFileSync("resume.xml");
-var resume = readResume(resumeXml);
-console.log("RESUME %j", resume);
+var cvJson = fs.readFileSync("cv.json");
+var cv = JSON.parse(cvJson);
+console.log("CV %j", cv);
 
-var html = swig.renderFile("template.html", resume);
+var html = swig.renderFile("cv.template", cv);
 console.log(html);
 
-fs.writeFileSync("resume.html", html);
+fs.writeFileSync("cv.html", html);
 
 var phantomArgs = [
 	path.join(__dirname, "rasterize.js"),
-	"resume.html",
-	"resume.pdf",
+	"cv.html",
+	"cv.pdf",
 	"A4"
 ];
 childProcess.execFile(phantomBinPath, phantomArgs, function(err, stdout, stderr) {
 	console.log("OK");
 });
-
-function readResume(resumeXml) {	
-	var xmlDoc = libxmljs.parseXml(resumeXml);
-
-	var personNode = xmlDoc.get("/resume/person");
-	var name = personNode.get("name").text();
-	var position = personNode.get("position").text();
-	var statement = personNode.get("statement").text();
-
-	var highlights = _.map(xmlDoc.find("/resume/highlights/highlight"), function(node) {
-		return node.text();
-	});
-
-	var projects = _.map(xmlDoc.find("/resume/projects/project"), function(node) {
-		return {
-			"name": node.get("name").text(),
-			"time": node.get("time").text(),
-			"url": (function() {
-				var url = node.get("url");
-				return url && url.text();
-			})(),
-			"sourceUrl": (function() {
-				var sourceUrl = node.get("source-url");
-				return sourceUrl && sourceUrl.text();
-			})(),
-			"description": node.get("description").text(),
-			"keywords": node.get("keywords").text(),
-			"responsibilities": _.map(node.find("responsibilities/responsibility"), function(node) {
-				return node.text();
-			})
-		};
-	});
-
-	return {
-		"name": name,
-		"position": position,
-		"statement": statement,
-		"highlights": highlights,
-		"projects": projects
-	};
-};
