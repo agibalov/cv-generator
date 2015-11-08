2e060554b9eebf934139badc78104347297e7721
loki2302 <loki2302@loki2302.me>
2015-11-09 00:19:06 +0300

    Updated a CV

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    swig_it: {
+      'dist': {
+        options: {
+          data: require('./cv.json')
+        },
+        src: ['./cv.html'],
+        dest: 'dist'
+      }
+    },
+
+    html_pdf: {
+      'dist': {
+        options: {
+          format: 'A4',
+          orientation: 'portrait',
+          quality: '75',
+          border: {
+            top: '0.5in',
+            right: '0.7in',
+            bottom: '0.5in',
+            left: '0.7in'
+          }
+        },
+        files: {
+          'dist/cv.pdf': ['dist/cv.html']
+        }
+      }
+    },
+
+    clean: {
+      'dist': ['dist']
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'dist',
+        repo: 'https://' + process.env.GH_TOKEN + '@github.com/loki2302/cv-generator.git'
+      },
+      src: ['**']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-swig-it');
+  grunt.loadNpmTasks('grunt-html-pdf');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['clean', 'swig_it', 'html_pdf']);
+  grunt.registerTask('deploy', ['default', 'gh-pages']);
+};
--- README.md
+++ README.md
@@ -1,12 +1,6 @@
 cv-generator
 ============
 
-1. `npm install`
-2. `npm start`
-3. see `cv.html` and `cv.pdf` generated
-
-Also (thanks to Travis):
-
 1. http://loki2302.me/cv-generator/cv.html
 2. http://loki2302.me/cv-generator/cv.pdf
 
